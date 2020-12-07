94b370a541331b53446548bab5857de2eef3a4c7
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2020-12-07 23:57:10 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,35 +3,35 @@
 let total = 0;
 
 while (true) {
-
   const numbers = [];
   let input = prompt('Введіть число');
-
-  console.log(input);
 
   if (input === null) {
     break;
   }
 
   input = Number(input);
-  numbers.push(input);
-  console.log(numbers);
+
+  // if (input === NaN) {
+  //   alert('Було написано не число, спробуйте ще раз');
+  //   continue;    
+  // }
+  // Почему код выше не работает?
 
   const notANumber = Number.isNaN(input);
-
   if (notANumber) {
     alert('Було написано не число, спробуйте ще раз');
     continue;
   }
 
-  // for (const number of numbers) {
-  //   total += number;
-  // }
+  numbers.push(input);
 
-  for (let i = 0; i < numbers.length; i += 1) {
-    total += numbers[i];
+  for (const number of numbers) {
+    total += number;
   }
-  
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   total += numbers[i];
+  // }
 }
 
 console.log(`Загальна сума чисел дорівнює ${total}`);
